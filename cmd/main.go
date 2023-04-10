9ffe19e7a4ff11e4e9196f5a4c741699802cc316
Jari Haapasaari <haapjari@gmail.com>
2023-04-10 18:32:25 +0300

    removed any correct errorhandling, we allow now the request to fail

--- cmd/main.go
+++ cmd/main.go
@@ -40,90 +40,90 @@ func main() {
 
         releases, err := g.GetTotalReleasesCount(owner, name)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         err = p.UpdateRows(db, url, "releases", releases)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         deployments, err := g.GetTotalDeploymentsCount(owner, name)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         err = p.UpdateRows(db, url, "deployments", deployments)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         contributors, err := g.GetTotalContributorsCount(owner, name)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         err = p.UpdateRows(db, url, "contributors", contributors)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         notifications, err := g.GetTotalNotificationCount(owner, name)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         err = p.UpdateRows(db, url, "notifications", notifications)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         openPullRequests, closedPullRequests, err := g.FetchAllPullRequests(owner, name) 
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         err = p.UpdateRows(db, url, "open_pulls", len(openPullRequests))
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         err = p.UpdateRows(db, url, "closed_pulls", len(closedPullRequests))
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         result, _, err := g.APIClient.Repositories.Get(g.APIClientContext, owner, name)    
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         forks := *result.ForksCount
 
         err = p.UpdateRows(db, url, "forks", forks)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         subs := *result.SubscribersCount
 
         err = p.UpdateRows(db, url, "subscribers", subs)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         events := result.GetNetworkCount()
 
         err = p.UpdateRows(db, url, "network_events", events)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
 
         watchers := result.GetWatchersCount()
 
         err = p.UpdateRows(db, url, "watchers", watchers)
         if err != nil {
-            log.Fatal(err)
+            log.Default().Printf("%s", err) 
         }
         
         remainingRepos := totalRepos - (i + 1)
